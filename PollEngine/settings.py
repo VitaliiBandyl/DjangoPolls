9f7311f9560808c9c2e71fdaac34a62f132b870c
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-06-13 22:04:26 +0300

    Added vote forms

--- PollEngine/settings.py
+++ PollEngine/settings.py
@@ -54,11 +54,6 @@ ROOT_URLCONF = 'PollEngine.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.jinja2.Jinja2",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
-        "APP_DIRS": True,
-    },
-    {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
